f35ea115432fc90bdf78b8e2f6feb283ea248eaf
wcy520 <17810234507@163.com>
2019-01-29 22:07:04 +0800

    shop文件夹
    Signed-off-by: wcy520 <17810234507@163.com>

--- elm/src/router/router.js
+++ elm/src/router/router.js
@@ -5,7 +5,7 @@ const city = r => require.ensure([], () => r(require('../page/city/city')), 'cit
 const msite = r => require.ensure([], () => r(require('../page/msite/msite')), 'msite')
 const food = r => require.ensure([], () => r(require('../page/food/food')), 'food')
 const search = r => require.ensure([], () => r(require('../page/search/search')), 'search')
-
+const shop = r => require.ensure([], () => r(require('../page/shop/shop')), 'shop')
 export default [{
     path: '/',
     component: App, //顶层路由，对应index.html
@@ -36,6 +36,22 @@ export default [{
         {
             path: '/search/:geohash',
             component: search
+        },
+        //商铺详情页
+        {
+            path: '/shop',
+            component: shop,
+            children: [{
+                path: 'foodDetail', //食品详情页
+                component: foodDetail,
+            }, {
+                path: 'shopDetail', //商铺详情页
+                component: shopDetail,
+                children: [{
+                    path: 'shopSafe', //商铺安全认证页
+                    component: shopSafe,
+                }, ]
+            }]
         },
     ]
 }]
